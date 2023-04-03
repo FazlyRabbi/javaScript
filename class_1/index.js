3564a885e57301460df290d279f2241140b6a316
kingdomofkushking1070bc <kingdomofkushking1070bc@gmail.com>
2023-04-03 11:23:54 +0600

    complete first classs

---
+++ class_1/index.js
@@ -0,0 +1,43 @@
+// function machin(dhan) {
+//   dhan = "caul";
+
+//   return dhan;
+// }
+
+// const cahul = machin();
+
+// const heading = document.querySelector(".heading");
+
+// heading.innerHTML = cahul;
+
+//  const array = [
+//     "rabbi",
+//     21
+//  ]
+
+//  console.log(array);
+//  console.log(array[0]);
+
+
+// const data = [
+//   { name: "rabbi", color: "red" },
+//   { name: "sakib", color: "green" },
+//   { name: "raju", color: "green" },
+//   { name: "rabbi", color: "green" },
+//   { name: "rabbi", color: "green" },
+// ];
+
+// const print = data.map((data, index) => {
+
+// return data
+
+// });
+
+//  console.dir(data);
+
+
+// console.log(print);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findLastIndex
