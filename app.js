cc4716d9b8dad76aaeac09b9ec6b9010c3510ead
kingdomofkushking1070bc <kingdomofkushking1070bc@gmail.com>
2023-04-05 10:41:48 +0600

    complete 2nd class

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+function updateClock() {
+    const timeWrapper =  document.querySelector("#time");    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    timeWrapper.textContent = `${hours}:${minutes}:${seconds}`;
+  }
+  
+  setInterval(updateClock, 1000);
+
+
+
--- class_1/index.js
+++ class_1/index.js
@@ -1,3 +1,22 @@
+// arrow function in javaScript
+// DOM
+// const statement = () => console.log("hello");
+
+// const heading = document.querySelector(".heading");
+// const btn = document.querySelector(".btn");
+// const text = document.querySelector(".text");
+
+// const changeTheText = () => {
+//   text.addEventListener("change", (e) => {
+//     console.log(e.target.value);
+//   });
+// };
+
+// btn.addEventListener("click", changeTheText);
+
+// console.dir(heading);
+// console.dir(btn);
+
 // function machin(dhan) {
 //   dhan = "caul";
 
@@ -18,7 +37,6 @@
 //  console.log(array);
 //  console.log(array[0]);
 
-
 // const data = [
 //   { name: "rabbi", color: "red" },
 //   { name: "sakib", color: "green" },
@@ -35,9 +53,8 @@
 
 //  console.dir(data);
 
-
 // console.log(print);
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findLastIndex
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findLastIndex
